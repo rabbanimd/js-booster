2f49431a1d8c70bf70e70d8380b996edaa4932d8
Md Rabbani <dev.rabbani@gmail.com>
2021-12-21 04:17:36 +0530

    initial commit

--- Quiz-app/index.js
+++ Quiz-app/index.js
@@ -39,7 +39,7 @@ const d = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 const restartBtn = document.getElementById('restart');
 let currentQuestion = 0;
-
+var answerList = [];
 loadQuiz();
 
 function loadQuiz(){
@@ -50,16 +50,40 @@ function loadQuiz(){
     c.innerText = quizData[currentQuestion].c;
     d.innerText = quizData[currentQuestion].d;
 }
+function getSelected() {
+    const answerEls = document.querySelectorAll('.answer');
+    let result = undefined;
+    answerEls.forEach((answerEl) =>{
+        if(answerEl.checked){
+            result = answerEl.id;
+            answerEl.checked = false;
+        }
+        
+    });
+    return result;
+    
+}
 
 submitBtn.addEventListener("click", ()=>{
+    
+    const answer = getSelected();    
+    answerList.push(answer);
     currentQuestion++;
-
+    
     if(currentQuestion < quizData.length){
         loadQuiz();
     }else{
         // todo : show results
-        alert("You finished the test");
+        let score = 0;
+        for(let i = 0; i<quizData.length; ++i)
+        {
+            if(answerList[i]===quizData[i].correct)score++;
+        }
+        alert("Test ended ! You scored = " + score+"/"+quizData.length);
+        currentQuestion = 0;
+        loadQuiz();
     }
+    
 });
 
 restartBtn.addEventListener("click", ()=>{
